6e7a9cf712d89f5fda229ed422b41f76193a200e
marisaa-kirisame <top.nep.413@gmail.com>
2023-10-28 14:35:28 +0200

    implemented simple empty ratatui terminal and added Song struct and also added a file opening dialog

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate rodio;
-extern crate tokio;
+extern crate rfd;
+extern crate id3;
 
+use id3::{Tag, TagLike};
+use rfd::FileDialog;
 use std::fs::File;
-use std::io::{BufReader, Split};
+use std::io::{BufRead, BufReader, Sink, Split, stderr};
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::*;
 use std::{io, thread};
@@ -10,10 +13,60 @@ use std::cmp::Ordering;
 use std::ops::{Add, Deref, Index, Sub};
 use std::path::Path;
 use std::time::Duration;
+use ratatui::backend::CrosstermBackend;
 use ratatui::buffer::Buffer;
+use ratatui::Terminal;
 use rodio::*;
+use anyhow::Result;
+use crossterm::event::{self, KeyCode, KeyEventKind};
+use crossterm::ExecutableCommand;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::widgets::{Block, Borders, Paragraph};
 
-// TODO: Figure out tokio or asynchronicity, I'm going to sleep
+struct Song {
+    title: String,
+    author: String
+}
+
+pub type Frame<'a> = ratatui::Frame<'a, CrosstermBackend<std::io::Stderr>>;
+struct ProgramState {
+    exit: bool,
+    mute: bool,
+    volume: f32,
+    current_playing: Option<Song>
+}
+
+impl Song {
+    fn new(title: String, author: String) -> Song {
+        Song {
+            title,
+            author
+        }
+    }
+
+    fn new_from_file(path: Path) -> Option<Song> {
+        let tag = Tag::read_from_path(path);
+        return Some(Song{
+            
+        })
+    }
+}
+
+impl ProgramState {
+    fn new() -> ProgramState {
+        ProgramState {
+            exit: false,
+            mute: false,
+            volume: 0.5,
+            current_playing: None
+        }
+    }
+
+    fn quit(&mut self) {
+        self.exit = true;
+    }
+}
+
 
 fn clamp(value: f32, lower_bounds: f32, upper_bounds: f32) -> f32 {
     if value < lower_bounds {
@@ -26,92 +79,88 @@ fn clamp(value: f32, lower_bounds: f32, upper_bounds: f32) -> f32 {
     return value;
 }
 
-fn main() {
-
+fn main() -> Result<()> {
     // Create volume variable
     let mut volume: f32 = 0.5;
     let mut exit = false;
-
 
     // Initialize rodio stream and sink for audio playback
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
 
     //let file1 = std::fs::File::open("assets/drill.mp3").unwrap();
-    //let file2 = std::fs::File::open("assets/everyone.mp3").unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
     //sink.append(rodio::Decoder::new(BufReader::new(file1)).unwrap());
 
+    startup()?;
+    let status = run(sink);
+    shutdown()?;
+    status?;
+    Ok(())
+}
+
+fn run(mut sink: rodio::Sink) -> Result<()> {
+    // ratatui terminal
+    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+
+    // app struct
+    let mut state = ProgramState::new();
+
     loop {
-        // Read line
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Error while reading string.");
+        terminal.draw( |f| {
+            ui(&mut state, f);
+        })?;
 
-        // Clean input up
-        input = input.replace("\r", "");
-        input = input.replace("\n", "");
+        update(&mut sink, &mut state)?;
 
-        // Separate input into commands
-        let mut command: Vec<&str> = input.split(' ').collect();
-
-        println!("Command is: {:?}\n", command);
-
-        // Match input
-        match command[0] {
-            // Quit on Q
-            "q" | "Q" | "quit" => {break},
-            // Mute on M
-            "m" | "M" | "mute" => {
-                if sink.volume() > 0.0 {
-                    sink.set_volume(0.0);
-                    println!("(i) Muted.\n")
-                }
-                else {
-                    sink.set_volume(1.0);
-                    println!("(i) Unmuted.\n")
-                }
-
-            },
-            "v" | "V" | "volume" => {
-                if command.len() >= 2 {
-                    sink.set_volume(match command[1].parse() {
-                        Ok(x) => clamp(x, 0.0, 1.0),
-                        Err(_) => sink.volume(),
-                    });
-                   println!("(i) Volume set to {}\n", sink.volume())
-                };
-            },
-            "p" | "P" | "play" => {
-                if command.len() >= 2{
-                    let file = File::open(command[1]);
-                    match file {
-                        Ok(f) => {
-                            println!("(i) {:?} queued. \n", f);
-                            sink.append(Decoder::new(BufReader::new(f)).unwrap());
-
-                        }
-                        Err(_) => {
-                            println!("(!) Couldn't open file.\n")
-                        }
-                    }
-
-                }
-            },
-            "h" | "H" | "help" | "HELP" | "what" => {
-                println!(
-"--- --- ---\n
-Welcome to rat-music! The following commands can be used to operate the program:\n
-- q/Q/quit -- Quits the program\n
-- m/M/mute -- Mutes and unmutes audio\n
-- v/V/volume [vol] -- sets the volume from 0.0 to 1.0\n
-- p/P/play [path] -- adds the file at [path] to the queue\n
---- --- ---\n")
-            }
-            _ => ()
+        if state.exit {
+            break;
         }
     }
 
+    return Ok(());
+}
 
+fn update(sink: &mut rodio::Sink, state: &mut ProgramState) -> Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Up => sink.set_volume(sink.volume()+0.1), // raise volume
+                    KeyCode::Down => sink.set_volume(sink.volume()-0.1), // lower vol
+                    KeyCode::Char('q') => state.quit(),
+                    KeyCode::Char('p') => {
+                        let file_path_buffer = FileDialog::new()
+                            .add_filter("music", &["mp3", "flac", "wav", "ogg"])
+                            .set_directory("/")
+                            .pick_file()
+                            .unwrap();
+                        let file = std::fs::File::open(file_path_buffer).unwrap();
+                        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+                    },
+                    _ => {},
+                }
+            }
+        }
+    }
+    Ok(())
+}
 
+fn ui(state: &mut ProgramState, frame: &mut Frame<'_>) {
+    frame.render_widget(
+        Paragraph::new("Now playing: nothing")
+                .block(Block::default().title("Welcome to rat-music!").borders(Borders::all())),
+                frame.size()
+    );
+}
 
+fn startup() -> Result<()> {
+    stderr().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    Ok(())
+}
 
+fn shutdown() -> Result<()> {
+    stderr().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
