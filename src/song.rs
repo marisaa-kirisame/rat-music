936a67a73236dcc91cde8f5ae7f6808f6a119a5f
marisaa-kirisame <top.nep.413@gmail.com>
2023-11-01 14:38:52 +0100

    resolved yesterday's todo: 1. the Song::new_from_file method has a match case for when the tag results in an error 2. the author field in Song is now optional and won't be used if id3 data doesn't provide one 3. the program doesn't panic when closing the file dialog or loading a file without id3 data

--- src/song.rs
+++ src/song.rs
@@ -1,14 +1,15 @@
+use std::ops::Deref;
 use std::path::Path;
 use id3::{Tag, TagLike};
 
 pub struct Song {
     title: String,
-    author: String
+    author: Option<String>
 }
 
 
 impl Song {
-    pub fn new(title: String, author: String) -> Song {
+    pub fn new(title: String, author: Option<String>) -> Song {
         Song {
             title,
             author
@@ -16,15 +17,30 @@ impl Song {
     }
 
     pub fn new_from_file(path: Box<Path>) -> Song {
-        let tag = Tag::read_from_path(path).unwrap(); // TODO: This panics on most files!!!!!!! Hahahahahaha fix this silly
-        Song {
-            title: tag.title().expect("Unknown").to_owned(),
-            author: tag.artist().expect("Unknown").to_owned()
+        let tag = Tag::read_from_path(path.clone());
+        match tag {
+            Ok(tag_content) => {
+                Song {
+                    title: tag_content.title().expect("Unknown").to_owned(),
+                    author: Some(tag_content.artist().expect("Unknown").to_owned())
+                }
+            },
+            Err(_) => {
+                Song {
+                    title: String::from(path.file_name().unwrap().to_str().unwrap()),
+                    author: None
+                }
+            }
         }
+
     }
 
 
     pub fn as_str(&self) -> String {
-        format!("{} - {}", self.author.clone(), self.title.clone())
+        match self.author.clone() {
+            Some(a) => { format!("{} - {}", a.clone(), self.title.clone()) },
+            None => { format!("{}", self.title.clone()) }
+        }
+
     }
 }
